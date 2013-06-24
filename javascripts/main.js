9772a380c2a04a3aaf2a0a57e885c7e83c102669
Josiah Gore <josiahgore@gmail.com>
2013-06-24 11:54:24 -0400

    adds a demo of the resizer to the page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+jQuery(function($) {
+  $('#container_width').val(parseInt($('#try_it_out_container').css('width')));
+
+  var resizeIt = function() {
+    $('#try_it_out_container')
+        .css('width', $('#container_width').val())
+        .css('height', $('#container_height').val())
+        .text($('#text_to_fit').text())
+        .resizeToFit();
+  }
+
+  $('#text_to_fit').blur(resizeIt);
+  $('#container_width,#container_height').spinner({
+    step: 25,
+    change: resizeIt,
+    stop: resizeIt
+  });
+
+  resizeIt();
+
+});
+
